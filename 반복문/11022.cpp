fc6ae688697c1d3252ac10afca5a744854bd6e3d
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-05-28 15:09:49 +0900

    Create 11022.cpp

---
+++ 반복문/11022.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int t, a, b;
+
+	cin >> t;
+
+	for (int i = 1; i <= t; i++)
+	{
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << endl;
+	}
+
+	return 0;
+}
