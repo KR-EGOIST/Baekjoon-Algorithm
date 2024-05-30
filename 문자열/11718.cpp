5230f6221fb963a39dff695d581145e3717725e2
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-05-30 16:21:33 +0900

    Update 11718.cpp

--- 문자열/11718.cpp
+++ 문자열/11718.cpp
@@ -1,3 +1,13 @@
+/*
+>> 연산자의 반환 값을 이용
+
+1. >> 연산자는 stream으로 부터 값을 읽어서 타겟 변수(cin)에 값을 반환한다.
+2. 이 때 stream에서 읽은 값이 타겟 변수의 type이 아니라면, stream은 invalid 상태로 mark된다.
+3. >> 연산자가 stream의 참조값(reference)을 cin에 반환하면, cin은 자신이 상속하는 istream 클래스의 explicit operator bool()로 인해 valid 상태의 stream은 true로, invalid 상태의 stream은 false로 변환하여 반환한다.
+즉, 주석에 써놓은 것처럼, cin >> (변수)는 입력값으로 잘못된 값(자료형 불일치)이 들어오면 false를 반환한다는 얘기다.
+
+*/
+
 #include<iostream>
 #include<string>
 using namespace std;
