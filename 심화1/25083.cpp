a37f5bddcc003b9d0926a41e67391735b2a58bdd
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-05-30 16:30:36 +0900

    Create 25083.cpp

---
+++ 심화1/25083.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout.tie(NULL);
+	  ios::sync_with_stdio(false);
+
+    cout << "         ,r'\"7\n";
+    cout << "r`-_   ,'  ,/\n";
+    cout << " \\. \". L_r'\n";
+    cout << "   `~\\/\n";
+    cout << "      |\n";
+    cout << "      |\n";
+
+    return 0;
+}
