aba2bffccb86ac5be3d08ea3985c2a13166b295b
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-06-13 09:55:14 +0900

    Create 2903.cpp

---
+++ 일반 수학 1/2903.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	int result = (int)pow(pow(2, n) + 1, 2);
+	cout << result << "\n";
+
+	return 0;
+}
