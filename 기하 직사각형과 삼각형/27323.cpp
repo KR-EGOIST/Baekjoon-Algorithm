ee101b3c0ec2a9891d8781371518b7a405bb28bf
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-06-16 20:25:09 +0900

    Create 27323.cpp

---
+++ 기하 직사각형과 삼각형/27323.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int a, b;
+	cin >> a >> b;
+
+	cout << a * b << "\n";
+
+	return 0;
+}
