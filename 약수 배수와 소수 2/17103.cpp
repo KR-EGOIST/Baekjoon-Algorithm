227c9b71125fea7a1e84e3e308a3e3e4fc3d01b6
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-08-22 18:23:41 +0900

    Update 17103.cpp

--- 약수 배수와 소수 2/17103.cpp
+++ 약수 배수와 소수 2/17103.cpp
@@ -1,57 +1,60 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 
 using namespace std;
 
-// 에라토스테네스의 체로 소수를 구하는 함수
-vector<bool> sieve(int max_num) {
-	vector<bool> isPrime(max_num + 1, true);
-	isPrime[0] = isPrime[1] = false;
+const int MAX_N = 1000000;  // N의 최대값
 
-	for (int i = 2; i * i <= max_num; i++) {
-		if (isPrime[i]) {
-			for (int j = i * i; j <= max_num; j += i) {
-				isPrime[j] = false;
-			}
-		}
-	}
+// 소수를 저장하는 배열
+vector<bool> isPrime(MAX_N + 1, true);
 
-	return isPrime;
+// 에라토스테네스의 체로 소수를 구하는 함수 (한 번만 계산)
+void sieve() {
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i <= MAX_N; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= MAX_N; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
 }
 
 // 골드바흐 파티션의 개수를 구하는 함수
 int countGoldbachPartitions(int n) {
-	vector<bool> isPrime = sieve(n);
-	int count = 0;
+    int count = 0;
 
-	// p1 <= p2 이므로 p1 <= n/2만 계산하면 된다.
-	for (int p1 = 2; p1 <= n / 2; ++p1) {
-		int p2 = n - p1;
-		if (isPrime[p1] && isPrime[p2]) {
-			count++;
-		}
-	}
+    // p1 <= p2 이므로 p1 <= n/2만 계산하면 된다.
+    for (int p1 = 2; p1 <= n / 2; ++p1) {
+        int p2 = n - p1;
+        if (isPrime[p1] && isPrime[p2]) {
+            count++;
+        }
+    }
 
-	return count;
+    return count;
 }
 
 int main() {
-	cin.tie(NULL);
-	cout.tie(NULL);
-	ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
 
-	int t;
-	cin >> t;
+    // 에라토스테네스의 체를 한 번만 실행
+    sieve();
 
-	int n;
-	while (t--)
-	{
-		cin >> n;
+    int t;
+    cin >> t;
 
-		int result = countGoldbachPartitions(n);
-		cout << result << "\n";
-	}
+    while (t--) {
+        int n;
+        cin >> n;
 
-	return 0;
+        int result = countGoldbachPartitions(n);
+        cout << result << "\n";
+    }
+
+    return 0;
 }
+
