0f42587e1620737bc926a8777da2c1d385cf240f
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-09-06 18:08:54 +0900

    Create 1037.cpp

---
+++ 심화2/1037.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    int result = v[0] * v[n - 1];
+
+    cout << result << "\n";
+    
+    return 0;
+}
